f51599a53544cef7945e074bfbeed5bca20bed28
Darsh <darsh2987@gmail.com>
2021-02-01 17:06:18 +0000

    rendering/styling all data requests(movie categories)components

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "../axios";
 
-function Row({ title, fetchUrl }) {
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -16,8 +18,14 @@ function Row({ title, fetchUrl }) {
   console.log(movies);
 
   return (
-    <div>
+    <div className="row">
       <h2>{title}</h2>
+
+      <div className="row-posters">
+        {movies.map((movie) => (
+          <img key={movie.id} className={`row-poster ${isLargeRow && "row-poster--large"}`} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
+        ))}
+      </div>
     </div>
   );
 }
