da09805f274cf193fdadc23fb77e6fb3ab7a6a7b
Darsh <darsh2987@gmail.com>
2021-02-01 17:12:21 +0000

    changed 'netflix' title to 'flixnet'

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import requests from "./requests";
 function App() {
   return (
     <div className="app">
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true} />
+      <Row title="FLIXNET ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true} />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
